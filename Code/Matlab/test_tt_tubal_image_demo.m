27620a117a748d953dc1306a9e1f08662fb05626
SalmanAhmadi-Asl <93009466+SalmanAhmadi-Asl@users.noreply.github.com>
2023-09-27 14:17:22 +0300

    Update test_tt_tubal_image_demo.m

--- Code/Matlab/test_tt_tubal_image_demo.m
+++ Code/Matlab/test_tt_tubal_image_demo.m
@@ -1,43 +1,13 @@
-%% Add lib
-addpath(genpath(pwd))
-
-%% Tubal TT
-% AnhHuy Phan
-
 clear all
 clc
 close all;
 
-%imfile = 'baboon.bmp';
-imfile = 'kodim3.bmp';
-imfile = ['kodim23.png'];
-%imfile = 'barbara.bmp';
-%imfile = 'airplane.bmp';
+imfile = 'airplane.bmp';
 %imfile = 'pepper.tiff';
 s1rgb = imread(imfile);
 [pp,imname,ex] = fileparts(imfile);
 Y = im2double(s1rgb) ;
 Y = imresize(Y,[512 512]);
-subplot(1,3,1)
- %imshow(Y)
-upperleft_pixel = [320, 150]; % another position of uperleft corner pixel
-box             = [80,   80];
-zoomfactor      = 2;
-zoom_position   = 'lr'; % position: 'lr' = "lower_right", 'll' = 'lower_left', 'ur' = 'uper_right', 'ul' = 'uper_left'
-I1=zoom_function(Y, upperleft_pixel, box, zoomfactor, zoom_position);
-% imagesc(I);
-% hold on 
-upperleft_pixel = [120, 205]; % another position of uperleft corner pixel
-box             = [80,   80];
-zoomfactor      = 2;
-zoom_position   = 'll'; % position: 'lr' = "lower_right", 'll' = 'lower_left', 'ur' = 'uper_right', 'ul' = 'uper_left'
-I2=zoom_function(I1, upperleft_pixel, box, zoomfactor, zoom_position);
-imagesc(I2)
-title('Original Image','FontSize',3)
-
-axis image
-axis off
-%%
 
 szY = size(Y);
 d = log2(szY(1));
@@ -52,10 +22,6 @@ szYt2 = [4*ones(1,d) 3];
 Yt = reshape(Yt,szYt2);
 
 ftensor2image = @(Yx) reshape(ipermute(reshape(Yx,szYt(ordix)),ordix),szY);
-
-%% 
-% 
-%% TT decomposition with specified ranks
 
 r = [1 2 6 9 35 40 51 16 4 1];
 
@@ -78,13 +44,12 @@ fprintf('PSNR %.2f dB \n', Perf_{2,2})
 fprintf('SSIM %.4f \n', Perf_{3,2})
 
 
-%fig = figure(1);
-%clf
-%imagesc(Yxm)
-%axis image
-%axis off
-%% Run DMRG two core update
-% 
+% fig = figure(4);
+% clf
+% imagesc(Yxm)
+% axis image
+% axis off
+
 
 opts = ttmps_a2cu();
 opts.compression = false;
@@ -106,22 +71,22 @@ fprintf('PSNR %.2f dB \n', Perf_{2,2})
 fprintf('SSIM %.4f \n', Perf_{3,2})
 
 
-%fig = figure(2);
-%clf
-%imagesc(Yxm)
-%axis image
-%axis off
-%% TubTT with approximation error given 
+% fig = figure(1);
+% clf
+% imagesc(Yxm)
+% axis image
+% axis off
+
 
 opts = ttmps_a2cu();
 opts.compression = false;
 
 %min  \|Y - X\|_F  <= noise_level^2 * numel(Y)
-relative_error = 0.08;
+relative_error = 0.15;
 opts.noise_level = relative_error^2*norm(Yt(:))^2;
 
-[Yt_dmrg,output] = tubtt_a2cu(Yt,[],opts); %proposed
-Yt_dmrgfull = full_tubtt(Yt_dmrg.U);       
+[Yt_dmrg,output] = tubtt_a2cu(Yt,[],opts);
+Yt_dmrgfull = full_tubtt(Yt_dmrg.U);
 
 %Visualize
 Yxm = ftensor2image(Yt_dmrgfull);
@@ -138,33 +103,18 @@ fprintf('PSNR %.2f dB \n', Perf_{2,2})
 fprintf('SSIM %.4f \n', Perf_{3,2})
 
 
-%fig = figure(3);
-subplot(1,3,3)
-%clf
-upperleft_pixel = [320, 150]; % another position of uperleft corner pixel
-box             = [80,   80];
-zoomfactor      = 2;
-zoom_position   = 'lr'; % position: 'lr' = "lower_right", 'll' = 'lower_left', 'ur' = 'uper_right', 'ul' = 'uper_left'
-I1=zoom_function(Yxm, upperleft_pixel, box, zoomfactor, zoom_position);
-% imagesc(I);
-% hold on 
-upperleft_pixel = [120, 205]; % another position of uperleft corner pixel
-box             = [80,   80];
-zoomfactor      = 2;
-zoom_position   = 'll'; % position: 'lr' = "lower_right", 'll' = 'lower_left', 'ur' = 'uper_right', 'ul' = 'uper_left'
-I2=zoom_function(I1, upperleft_pixel, box, zoomfactor, zoom_position);
-imagesc(I2)
-title('Proposed model','FontSize', 3)
+fig = figure(1);
+clf
+imagesc(Yxm)
+title('Proposed algorithm')
 axis image
 axis off
-
-%% TT 
 
 opts = ttmps_a2cu();
 opts.compression = false;
 
 %min  \|Y - X\|_F  <= noise_level^2 * numel(Y)
-relative_error = 0.08;
+relative_error = 0.15;
 opts.noise_level = relative_error^2*norm(Yt(:))^2;
 
 [Ytx2,output] = ttmps_a2cu(Yt,[],opts);
@@ -184,26 +134,13 @@ fprintf('PSNR %.2f dB \n', Perf_{2,2})
 fprintf('SSIM %.4f \n', Perf_{3,2})
 
 
-%fig = figure(4);
-subplot(1,3,2)
-%clf
-upperleft_pixel = [320, 150]; % another position of uperleft corner pixel
-box             = [80,   80];
-zoomfactor      = 2;
-zoom_position   = 'lr'; % position: 'lr' = "lower_right", 'll' = 'lower_left', 'ur' = 'uper_right', 'ul' = 'uper_left'
-I1=zoom_function(Yxm, upperleft_pixel, box, zoomfactor, zoom_position);
-% imagesc(I);
-% hold on 
-upperleft_pixel = [120, 205]; % another position of uperleft corner pixel
-box             = [80,   80];
-zoomfactor      = 2;
-zoom_position   = 'll'; % position: 'lr' = "lower_right", 'll' = 'lower_left', 'ur' = 'uper_right', 'ul' = 'uper_left'
-I2=zoom_function(I1, upperleft_pixel, box, zoomfactor, zoom_position);
-imagesc(I2)
-title('TT-Based','FontSize', 3)
+fig = figure(2);
+clf
+imagesc(Yxm)
+title('TT-based algorithm')
 axis image
 axis off
-%%
+
 function Perf_ = eval_rec(Y,Ym)
 
 % Assess performance of denoising
