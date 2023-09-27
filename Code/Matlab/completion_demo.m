fc87a18301b44cfbf92a3b3958f3775f5314214a
SalmanAhmadi-Asl <93009466+SalmanAhmadi-Asl@users.noreply.github.com>
2023-09-27 11:51:14 +0300

    Update completion_demo.m

--- Code/Matlab/completion_demo.m
+++ Code/Matlab/completion_demo.m
@@ -1,8 +1,8 @@
 clc;clear all;close all
 
 %imfile = 'baboon.bmp';
-imfile = 'kodim3.bmp';
-imfile = ['kodim23.png'];
+imfile = 'kodim4.png';
+%imfile = ['kodim23.png'];
 %imfile = 'barbara.bmp';
 %imfile = 'airplane.bmp';
 %imfile = 'pepper.tiff';
@@ -16,11 +16,11 @@ Y = imresize(Y,[512 512]);
 %title('Original Image','FontSize',3)
 
 
-mr=0.80;
+mr=0.70;
 S=size(Y);
 Q = gen_W(S,mr);
 Y_miss=Q.*Y;
-%%%%%%
+%% %%%%
 
 szY = size(Y);
 d = log2(szY(1));
@@ -47,7 +47,7 @@ r = [1 2 6 14 14 14 14 14 4 1];
 
 YY=randn(size(Yt));
 
-for i=1:500
+for i=1:50
 % TT-SVD algorithm with ranks given
 YYt = tubttsvd(YY,r);
 YY = full_tubtt(YYt);
@@ -55,24 +55,49 @@ YY=YY.*(~Qt)+Yt_miss;
 Yh = ftensor2image(YY);
 imagesc(Yh)
 drawnow
+fprintf('Iteration: %d', i)
 eval_rec(Yh,Y)
 end
 %%
-% YY=randn(size(Yt));
-% for i=1:500
-% % TT-SVD algorithm with ranks given
-% [ut,st,vt] = H_tsvd(YY);
-% u_t=ut(:,:,:,:,:,:,:,:,:,:);
-% s_t=st(:,:,:,:,:,:,:,:,:,:);
-% s_t=st(:,:,:,:,:,:,:,:,:,:);
-% YY = H_tprod(H_tprod(ut,st),H_tran(vt));
-% YY=YY.*(~Qt)+Yt_miss;
-% Yh = ftensor2image(YY);
-% imagesc(Yh)
-% drawnow
-% eval_rec(Yh,Y)
-% end
+Yt=reshape(Y,[64,64,64,3]);
+Qt=reshape(Q,[64,64,64,3]);
+Yt_miss=reshape(Y_miss,[64,64,64,3]);
+ R=3;
+YY=randn(size(Yt));
+for i=1:50
+% TT-SVD algorithm with ranks given
+[ut,st,vt] = H_tsvd(YY);
+% YYY = H_tprod(H_tprod(ut,st),H_tran(vt));
+% u_t=ut(:,1:3,:,:,:,:,:,:,:,:);
+% s_t=st(1:3,1:3,:,:,:,:,:,:,:,:);
+% v_t=vt(:,1:3,:,:,:,:,:,:,:,:);
+u_t=ut(:,1:R,:,:);
+s_t=st(1:R,1:R,:,:);
+v_t=vt(:,1:R,:,:);
+YY = H_tprod(H_tprod(u_t,s_t),H_tran(v_t));
+YY=YY.*(~Qt)+Yt_miss;
+Yh_2 = reshape(YY,[512,512,3]);
+imagesc(Yh_2)
+drawnow
+fprintf('Iteration: %d', i)
+eval_rec(Yh,Y)
+end
 
+subplot(1,4,1)
+imshow(Y)
+%title('Original Image','FontSize',20)
+subplot(1,4,2)
+imshow(Y_miss)
+%title('Missing Image','FontSize',20)
+subplot(1,4,3)
+imshow(Yh)
+%title('Reconstructed Image (Proposed)','FontSize',20)
+subplot(1,4,4)
+imshow(Yh_2)
+%title('Reconstructed Image (T-SVD)','FontSize',20)
+%%
+eval_rec(Yh_2,Y)
+eval_rec(Yh,Y)
 
 function Perf_ = eval_rec(Y,Ym)
 
